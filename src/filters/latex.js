6d81f8f05ed8be683ce8b6b0a438356b754b9019
Avi Mehra <pihart@users.noreply.github.com>
2024-02-22 18:55:20 -0800

    Support latex bracket args.
    Resolves https://github.com/pihart/tex/issues/104

--- src/filters/latex.js
+++ src/filters/latex.js
@@ -10,17 +10,26 @@ processLatex = (ele) => {
   if (!["Div", "CodeBlock"].includes(ele.t)) return;
 
   const [attr, childrenOrCode] = ele.c; // when Div, we have children: Block[]; when CodeBlock, we have code: string
-  const [id, classList] = attr;
+
+  /**
+   * @type {[string, string[], [string, string][]]}
+   * attrs is key-value pairs
+   */
+  const [id, classList, attrs] = attr;
+  const attrsMap = Object.fromEntries(attrs);
+
   if (classList.length !== 1) return; // Not implemented
 
   const [env] = classList; // Assuming that, by the next line, this is a latex env, this is that env; the name is thus appropriate
   const labelText = id ? `\\label{${id}}` : "";
+  const bracketArgs = attrsMap["bracket-args"] ?? attrsMap["sbr"];
+  const bracketArgsStr = bracketArgs ? `[${bracketArgs}]` : "";
 
   if (ele.t === "Div")
     return Div(
         ["", [], []],
         [
-          RawBlock("latex", `\\begin{${env}}${labelText}`),
+          RawBlock("latex", `\\begin{${env}}${bracketArgsStr}${labelText}`),
           ...childrenOrCode, // Leave this to get processed as markdown
           RawBlock("latex", `\\end{${env}}`),
         ]
